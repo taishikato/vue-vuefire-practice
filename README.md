f1ef3b4f4a3787a1431ebd4ef462b5bfb50a4a4e
taishikato <taishi.k0903@gmail.com>
2017-11-19 19:56:29 +0900

    toaster以外完成

--- README.md
+++ README.md
@@ -27,3 +27,9 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## Others
+
+```bash
+npm install toaster --save
+```
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,30 @@
     <div class="page-header">
       <h1>Vue.js 2 & Firebase Sample Application</h1>
     </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">Url:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3>Books List</h3>
@@ -10,7 +34,31 @@
       <div class="panel-body">
         <table class="table table-striped">
           <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+              <th>
+                Delete
+              </th>
+            </tr>
           </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a v-bind:href="book.url">{{book.title}}</a>
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+              <td>
+                <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
@@ -38,6 +86,30 @@ export default {
   name: 'app',
   firebase: {
     books: bookRef
+  },
+  data () {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function () {
+      bookRef.push(this.newBook);
+      this.newBook.title = '';
+      this.newBook.author = '';
+      this.newBook.url = '';
+    },
+    removeBook: function(book) {
+      console.log(book['.key']);
+      bookRef.child(book['.key']).remove();
+      this.newBook.title = '';
+      this.newBook.author = '';
+      this.newBook.url = '';
+    }
   }
 }
 </script>
@@ -47,7 +119,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
